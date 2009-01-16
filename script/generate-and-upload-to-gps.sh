5f8978099626cf33d4f2696c19a7ef22c1047ab6
Ævar Arnfjörð Bjarmason <avar@aoeu.(none)>
2009-01-16 16:29:49 +0000

    what I use at aoeu at the moment

--- script/generate-and-upload-to-gps.sh
+++ script/generate-and-upload-to-gps.sh
@@ -3,11 +3,12 @@ rm -v *.img *.tdb
 
 mkdir -p osm/is
 
-wget http://osm.nix.is/latest/cyclemap/13314531.img -O osm/is/cyclemap.img
 wget http://osm.nix.is/latest/default/13314530.img  -O osm/is/default.img
+wget http://osm.nix.is/latest/cyclemap/13314531.img -O osm/is/cyclemap.img
+wget http://osm.nix.is/latest/routeable/13314532.img  -O osm/is/routeable.img
 
 java -jar ~/src/mkgmap/dist/mkgmap.jar --latin1 --gmapsupp Island-Topo/7733011[23456].img osm/is/*.img
 
-sudo mount /dev/sd?1 /mnt/gps/
-sudo cp -v gmapsupp.img /mnt/gps/Garmin/
-sudo umount /mnt/gps
+#sudo mount /dev/sd?1 /mnt/gps/
+sudo cp -v gmapsupp.img /media/disk/Garmin/
+#sudo umount /mnt/gps

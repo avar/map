c1f11eedb37846e5b955589367f98ff5cc270445
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2009-07-03 16:32:19 +0000

    Generate Germany too and upload it

--- script/generate-and-upload-to-gps.sh
+++ script/generate-and-upload-to-gps.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 rm -v *.img *.tdb
 
-rm -rfv osm/is
-mkdir -p osm/is
+rm -rfv osm/is osm/de
+mkdir -p osm/is osm/de
 
 wget http://osm.nix.is/latest/default/13314530.img  -O osm/is/default.img
-#wget http://osm.nix.is/latest/routeable/13314532.img  -O osm/is/routeable.img
-#wget http://osm.nix.is/latest/cyclemap/13314531.img -O osm/is/cyclemap.img
-
+wget http://osm.nix.is/berlin/latest/default/13314540.img  -O osm/de/default.img
 
 #iceland_maps="Island-Topo/7733010[123456].img"
 iceland_maps=""
 iceland_contours="Island-Topo/7733011[23456].img"
 iceland_osm="osm/is/*.img"
+germany_osm="osm/de/*.img"
 
-java -jar ~/src/mkgmap/dist/mkgmap.jar --latin1 --gmapsupp $iceland_maps $iceland_contours $iceland_osm
+java -jar ~/src/mkgmap/dist/mkgmap.jar --latin1 --gmapsupp $iceland_maps $iceland_contours $iceland_osm $germany_osm
 
 #sudo mount /dev/sd?1 /mnt/gps/
-sudo cp -v gmapsupp.img /media/disk/Garmin/
+mkdir -p /media/gps/Garmin
+sudo cp -v gmapsupp.img /media/gps/Garmin/
 #sudo umount /mnt/gps
